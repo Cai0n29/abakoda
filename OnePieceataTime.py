7cf83b37304d9776626d1686e96097e1bc18b5ac
Cai0n29 <capellakratos@gmail.com>
2023-08-22 21:16:53 +0800

    ABaKoDa 2022 Round 4

---
+++ OnePieceataTime.py
@@ -0,0 +1,36 @@
+n = input().upper()
+if n == 'I':
+    print("#.")
+    print("#.")
+    print("#.")
+    print("#.")
+elif n == 'J':
+    print(".#")
+    print(".#")
+    print("##")
+    print("..")
+elif n == 'O':
+    print("##")
+    print("##")
+    print("..")
+    print("..")
+elif n == 'L':
+    print("#.")
+    print("#.")
+    print("##")
+    print("..")
+elif n == 'S':
+    print("#.")
+    print("##")
+    print(".#")
+    print("..")
+elif n == 'T':
+    print("#.")
+    print("##")
+    print("#.")
+    print("..")
+elif n == 'Z':
+    print(".#")
+    print("##")
+    print("#.")
+    print("..")
